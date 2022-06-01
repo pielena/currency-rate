ec1d043dc4df2cf4530b070059b9b5dbbdc3a49b
Elena Pilyugina <elena.pilyugina.job@gmail.com>
2022-06-01 16:25:35 +0300

    create develop

--- src/main/java/com/currency/currencyrate/CurrencyRateApplication.java
+++ src/main/java/com/currency/currencyrate/CurrencyRateApplication.java
@@ -9,5 +9,4 @@ public class CurrencyRateApplication {
     public static void main(String[] args) {
         SpringApplication.run(CurrencyRateApplication.class, args);
     }
-
 }
--- src/test/java/com/currency/currencyrate/CurrencyRateApplicationTests.java
+++ src/test/java/com/currency/currencyrate/CurrencyRateApplicationTests.java
@@ -9,5 +9,4 @@ class CurrencyRateApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
